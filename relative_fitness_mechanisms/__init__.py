ef4e7cd0cc5be092a783c25d122fdb129e3cfe0d
marlinfiggins <marlinfiggins@gmail.com>
2024-01-12 11:40:53 -0800

    HSGP implementation pt2

--- relative_fitness_mechanisms/__init__.py
+++ relative_fitness_mechanisms/__init__.py
@@ -1,4 +1,5 @@
 from .relative_fitness_gp import *
+from .relative_fitness_hsgp import *
 from .latent_immunity_relative_fitness import RelativeFitnessDR
 from .frequency_simulation import *
 from .ode_models import *
