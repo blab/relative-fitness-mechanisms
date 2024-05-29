4534e23be6024df21622f7f4625054eed216f5d5
marlinfiggins <marlinfiggins@gmail.com>
2024-05-29 15:36:27 -0700

    Formatting

--- relative_fitness_mechanisms/relative_fitness_hsgp.py
+++ relative_fitness_mechanisms/relative_fitness_hsgp.py
@@ -1,17 +1,16 @@
 from functools import partial
 from typing import Optional
+
 import evofr as ef
 import jax
-import numpy as np
 import jax.numpy as jnp
-from jax.nn import softmax
-
+import numpy as np
 import numpyro
 import numpyro.distributions as dist
-
-from jax import jit, vmap, Array
-import jax.numpy as jnp
+from jax import Array, jit, vmap
+from jax.nn import softmax
 from jax.scipy.special import gammaln
+
 
 class HSGaussianProcess:
     """
@@ -37,9 +36,9 @@ class HSGaussianProcess:
 
     @staticmethod
     def phi_matrix(L: float, js: Array, x: Array):
-        phi_mapped = jit(vmap(
-            HSGaussianProcess.phi, in_axes=(None, 0, None), out_axes=-1
-        )) #Map across m values
+        phi_mapped = jit(
+            vmap(HSGaussianProcess.phi, in_axes=(None, 0, None), out_axes=-1)
+        )  # Map across m values
         return phi_mapped(L, js, x)
 
     def make_features(self, ts) -> Array:
@@ -48,6 +47,7 @@ class HSGaussianProcess:
         phi = self.phi_matrix(self.L, ms, ts)
         return phi
 
+
 def assign_priors(name, val, default: dist.Distribution):
     if val is None:
         return numpyro.sample(name, default)
@@ -55,6 +55,7 @@ def assign_priors(name, val, default: dist.Distribution):
         return numpyro.sample(name, val)
     else:
         return val
+
 
 class SquaredExponential(HSGaussianProcess):
     def __init__(
@@ -66,25 +67,20 @@ class SquaredExponential(HSGaussianProcess):
     ):
         """
         HSGP using a squared exponential or radial basis function kernel.
-        
+
         alpha: marginal variance
         rho: length-scale
         L: ...
 
         """
-        self.alpha = alpha 
-        self.rho = rho 
-        super().__init__(L= L if L else 10.0, num_basis=num_basis if num_basis else 50)
+        self.alpha = alpha
+        self.rho = rho
+        super().__init__(L=L if L else 10.0, num_basis=num_basis if num_basis else 50)
 
     @staticmethod
     def spd(alpha: float, rho: float, w: Array):
-        return (
-            alpha
-            * jnp.sqrt(2 * jnp.pi)
-            * rho
-            * jnp.exp(-0.5 * jnp.square(rho* w))
-        )
-    
+        return alpha * jnp.sqrt(2 * jnp.pi) * rho * jnp.exp(-0.5 * jnp.square(rho * w))
+
     def model(self):
         alpha = assign_priors("alpha", self.alpha, default=dist.HalfNormal(1e-3))
         rho = assign_priors("rho", self.rho, default=dist.HalfNormal(25))
@@ -102,8 +98,8 @@ class Matern(HSGaussianProcess):
     ):
         self.alpha = alpha
         self.rho = rho
-        self.nu = nu if nu else 5/2
-        super().__init__(L= L if L else 10.0, num_basis=num_basis if num_basis else 50)
+        self.nu = nu if nu else 5 / 2
+        super().__init__(L=L if L else 10.0, num_basis=num_basis if num_basis else 50)
 
     @staticmethod
     def spd(alpha: float, rho: float, nu: float, w: Array):
@@ -141,29 +137,38 @@ class SpectralMixture(HSGaussianProcess):
         self.mixture_weights = mixture_weights
         self.mixture_means = mixture_means
         self.mixture_sigmas = mixture_sigmas
-        super().__init__(L= L if L else 10.0, num_basis=num_basis if num_basis else 50)
+        super().__init__(L=L if L else 10.0, num_basis=num_basis if num_basis else 50)
 
     @staticmethod
-    def spd(mixture_weights: Array, mixture_means: Array, mixture_sigmas: Array, w: Array):
+    def spd(
+        mixture_weights: Array, mixture_means: Array, mixture_sigmas: Array, w: Array
+    ):
         coefs = jnp.reciprocal(jnp.sqrt(2 * jnp.pi)) / mixture_sigmas
-        args = jnp.square((w[..., None] - mixture_means)/ mixture_sigmas)
+        args = jnp.square((w[..., None] - mixture_means) / mixture_sigmas)
         return (coefs * mixture_weights * jnp.exp(-0.5 * args)).sum(axis=-1)
 
     def model(self):
-        mixture_weights = assign_priors("mixture_weights", self.mixture_weights, default=dist.Dirichlet(jnp.ones(self.num_components)))
+        mixture_weights = assign_priors(
+            "mixture_weights",
+            self.mixture_weights,
+            default=dist.Dirichlet(jnp.ones(self.num_components)),
+        )
         mixture_means = assign_priors(
-            "mixture_means", 
-            self.mixture_means, 
+            "mixture_means",
+            self.mixture_means,
             default=dist.TransformedDistribution(
-                dist.Normal(0,1.0).expand((self.num_components,)),
-                dist.transforms.OrderedTransform())
+                dist.Normal(0, 1.0).expand((self.num_components,)),
+                dist.transforms.OrderedTransform(),
+            ),
         )
         mixture_sigmas = assign_priors(
             "mixture_sigmas",
             self.mixture_sigmas,
-            default=dist.HalfNormal(0.5).expand((self.num_components,))
+            default=dist.HalfNormal(0.5).expand((self.num_components,)),
         )
-        return self.spd(mixture_weights, mixture_means, mixture_sigmas, jnp.sqrt(self.lams))
+        return self.spd(
+            mixture_weights, mixture_means, mixture_sigmas, jnp.sqrt(self.lams)
+        )
 
 
 def not_yet_observed(seq_counts):
@@ -175,12 +180,13 @@ def not_yet_observed(seq_counts):
     for v in range(V):
         for t in range(1, T):
             # If we haven't seen yet, check that we still havent
-            if never_seen[t-1, v]:
-                never_seen[t,v] = seq_counts[t,v] == 0
+            if never_seen[t - 1, v]:
+                never_seen[t, v] = seq_counts[t, v] == 0
             # If we have seen it, we know it's 0
             else:
-                never_seen[t,v] = 0
+                never_seen[t, v] = 0
     return never_seen
+
 
 def relative_fitness_hsgp_numpyro(
     seq_counts, N, hsgp, tau=None, pred=False, var_names=None
@@ -196,10 +202,13 @@ def relative_fitness_hsgp_numpyro(
 
     with numpyro.plate("variant", N_variants - 1):
         _init_logit = numpyro.sample("init_logit", dist.Normal(0, 6.0))
-        intercept =  numpyro.sample("intercept", dist.Normal(0, 1.0))
+        intercept = numpyro.sample("intercept", dist.Normal(0, 1.0))
         with numpyro.plate("num_basis", num_basis):
             beta = numpyro.sample("beta", dist.Normal(0, 1))
-    _fitness = numpyro.deterministic("delta", jnp.einsum("tj, jv -> tv", phi, spd[..., None] * beta) + intercept[None, :])
+    _fitness = numpyro.deterministic(
+        "delta",
+        jnp.einsum("tj, jv -> tv", phi, spd[..., None] * beta) + intercept[None, :],
+    )
     fitness = jnp.hstack((_fitness, jnp.zeros((N_time, 1))))
 
     # Sum fitness to get dynamics over time
@@ -227,6 +236,7 @@ def relative_fitness_hsgp_numpyro(
             "ga", jnp.exp(fitness[:, :-1] * tau)
         )  # Last row corresponds to linear predictor / growth advantage
 
+
 class RelativeFitnessHSGP(ef.ModelSpec):
     def __init__(
         self,
@@ -251,9 +261,7 @@ class RelativeFitnessHSGP(ef.ModelSpec):
         """
         Abstract away NUTS stuff in Evofr and numpyro for quick usage.
         """
-        inference_method = ef.InferNUTS(
-            num_warmup=num_warmup, num_samples=num_samples
-        )
+        inference_method = ef.InferNUTS(num_warmup=num_warmup, num_samples=num_samples)
         return inference_method.fit(self, data)
 
     def forecast_mcmc(self, samples, forecast_L):
@@ -267,20 +275,19 @@ class RelativeFitnessHSGP(ef.ModelSpec):
         # Forecast relative fitness
         def _delta_forecast(spd, beta):
             return jnp.einsum("tj, jv -> tv", phi_forecast, spd[..., None] * beta)
-        samples["delta_forecast"] = vmap(_delta_forecast, in_axes=(0, 0))(samples["sqrt_spd"], samples["beta"])
+
+        samples["delta_forecast"] = vmap(_delta_forecast, in_axes=(0, 0))(
+            samples["sqrt_spd"], samples["beta"]
+        )
 
         # Forecast frequency
         def _forecast_freq(fitness, frequency):
             init_freq = jnp.log(frequency[-1, :])  # Last known frequency
-            _fitness = jnp.concatenate(
-                (fitness, jnp.zeros((forecast_L,1))), axis=-1
-            )
+            _fitness = jnp.concatenate((fitness, jnp.zeros((forecast_L, 1))), axis=-1)
             cum_fitness = jnp.cumsum(_fitness.at[0, :].set(0), axis=0)
             return softmax(cum_fitness + init_freq, axis=-1)
 
-        vmap_forecast_freq = jax.vmap(
-            _forecast_freq, in_axes=(0, 0), out_axes=0
-        )
+        vmap_forecast_freq = jax.vmap(_forecast_freq, in_axes=(0, 0), out_axes=0)
         samples["freq_forecast"] = vmap_forecast_freq(
             samples["delta_forecast"], samples["freq"]
         )
